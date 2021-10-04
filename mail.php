6f7cfbb0b47350789b8c43292be28c4009725a63
pushpakkaloge <pushpakkaloge@gmail.com>
2021-10-04 20:36:52 +0530

    first commit

--- mail.php
+++ mail.php
@@ -6,8 +6,8 @@ $message= $_POST['message'];
 $to = "pushpakkaloge@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From: pushpakkaloge@gmail.com" . "\r\n" .
+"CC: pushpakkaloge@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
